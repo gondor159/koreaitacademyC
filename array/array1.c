862f9c418ce34cbf2002b45ca544271c9fbbbeae
sjh <lacls159@gmail.com>
2021-06-27 22:10:12 -0400

    adding array

---
+++ array/array1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int x[5];
+	int n;
+
+	for(n = 0; n < 5; n++) {
+		x[n] = n * 2;
+	}
+
+	for(n = 0; n < 5; n++) {
+		printf("%d - %d\n", n, x[n]);
+	}
+
+	return 0;
+}
